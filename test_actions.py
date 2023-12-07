9b7df67ce69a5211b033e9bb0185250de18bd21a
BASSINAS <56053848+BASSINAS@users.noreply.github.com>
2023-12-07 23:43:33 +0100

    Update test_actions.py
    update

--- test_actions.py
+++ test_actions.py
@@ -1,27 +1,17 @@
-from typing import Any
-import pytest
 import re
 from playwright.sync_api import Page, expect
 
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
 
-class Application(Page):
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwright"))
 
-    def __init__(self):
-        self.page_accueil = Page()
-        
-        
-    def loadpage(self):
-        self.page_accueil.goto("https://google.com")  
+def test_get_started_link(page: Page):
+    page.goto("https://playwright.dev/")
 
+    # Click the get started link.
+    page.get_by_role("link", name="Get started").click()
 
-def test_loadpage():
-    application = Application()
-    application.loadpage() 
-    expect(application.get_by_role('button', name='Recherche Google')).to_be_visible
-
-
-
-    
-           
-
-    
+    # Expects page to have a heading with the name of Installation.
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
