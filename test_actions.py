4a9d988b96311096765386cd0b2e79737d2099d9
BASSINAS <56053848+BASSINAS@users.noreply.github.com>
2023-12-07 21:34:32 +0100

    first_test
    example

---
+++ test_actions.py
@@ -0,0 +1,27 @@
+from typing import Any
+import pytest
+import re
+from playwright.sync_api import Page, expect
+
+
+class Application(Page):
+
+    def __init__(self):
+        self.page_accueil = Page()
+        
+        
+    def loadpage(self):
+        self.page_accueil.goto("https://google.com")  
+
+
+def test_loadpage():
+    application = Application()
+    application.loadpage() 
+    expect(application.get_by_role('button', name='Recherche Google')).to_be_visible
+
+
+
+    
+           
+
+    
